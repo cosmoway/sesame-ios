8f20efc2564a6f693de07cb194b04098517903fe
Yuuki Shimizu <shimizu@cosmoway.net>
2016-03-10 22:13:12 +0900

    ディレクトリを 1つ上の階層に移動

---
+++ sesame-iosTests/sesame_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  sesame_iosTests.swift
+//  sesame-iosTests
+//
+//  Created by 坂野健 on 2016/02/23.
+//  Copyright © 2016年 坂野健. All rights reserved.
+//
+
+import XCTest
+@testable import sesame_ios
+
+class sesame_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
